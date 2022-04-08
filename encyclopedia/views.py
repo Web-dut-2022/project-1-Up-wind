59211c3a2d888d8ea5e70022afbe147447ac9004
Upw1nd <lou-hs@hotmail.com>
2022-04-08 12:23:26 +0800

    Update index and entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +11,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry_md = util.get_entry(title)
+    if entry_md == None:
+        return render(request, "encyclopedia/error.html", {"message": "Page not found."})
+    else:
+        entry_html = Markdown().convert(entry_md)
+        context = {
+            "tilte": title,
+            "entry": entry_html
+        }
+        return render(request, "encyclopedia/entry.html", context)
+
+def search(request):
+    return HttpResponseRedirect(reverse("index"))
+    # return render(request, "encyclopedia/search.html", context)
